e2a0c7bf7df27350dd06d512bf8214216f1455ae
Hok Kan Cheung <hokkan@connect.hku.hk>
2025-09-11 08:55:33 -0400

    Update Readme & pack-repo.py

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # Repo-Context-Packager
+
 Pack Git Repository into a text file for use in LLM
+
+## Instruction for Windows Users:
+
+### Step 1
+
+Check if Python is installed in the system by the following command:
+`python --version`
+or
+`py --version`
+
+If Python is not installed, visit https://www.python.org/ to download and install
+
+### Step 2
+
+Download pack-repo.py to your local machine
+
+### Step 3
+
+Run the script by the following command:
+`python pack-repo.py repo-path [-o] [output-file-name]`
+or
+`py pack-repo.py repo-path [-o] [output-file-name]`
+
+## Instruction for Mac/Linux Users:
+
+### Step 1
+
+Check if Python is installed in the system by the following command:
+`python3 --version`
+
+If Python is not installed, visit https://www.python.org/ to download and install
+
+### Step 2
+
+Download pack-repo.py to your local machine
+
+### Step 3
+
+Run the script by the following command:
+`python3 pack-repo.py repo-path [-o] [output-file-name]`
--- pack-repo.py
+++ pack-repo.py
@@ -10,8 +10,32 @@ def write_file_location(f, input_path):
     print("Writing file system location...")
     abs_path = Path(input_path).resolve()
     with f.open("a") as f:
-        f.write("## File System Location\n")
+        f.write("## File System Location\n\n")
         f.write(str(abs_path) + "\n\n")
+
+def write_git_info(f):
+    print("***Codes for writing Git Info to be implemented***")
+    with f.open("a") as f:
+        f.write("## Git Info\n\n")
+        f.write("***To be implemented***\n\n")
+
+def write_struct_tree(f):
+    print("***Codes for writing Project Structure Tree to be implemented***")
+    with f.open("a") as f:
+        f.write("## Structure\n\n")
+        f.write("***To be implemented***\n\n")
+
+def write_file_contents(f):
+    print("***Codes for writing File Content to be implemented***")
+    with f.open("a") as f:
+        f.write("## File Contents\n\n")
+        f.write("***To be implemented***\n\n")
+
+def write_summary(f):
+    print("***Codes for writing Summary Statistics to be implemented***")
+    with f.open("a") as f:
+        f.write("## Summary\n\n")
+        f.write("***To be implemented***\n\n")
 
 if __name__ == "__main__":
     #set argument parser
@@ -28,6 +52,10 @@ if __name__ == "__main__":
     #write to file
     print("Writing file...")
     write_file_location(f, args.path)
+    write_git_info(f)
+    write_struct_tree(f)
+    write_file_contents(f)
+    write_summary(f)
 
     #program complete
     print(f'All information is saved in "{args.output}"')
