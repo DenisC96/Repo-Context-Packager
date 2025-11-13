e5ee5e77d777230e8170e5d9ee39d234609d3775
Hok Kan Cheung <hokkan@connect.hku.hk>
2025-11-13 01:13:32 -0500

    Update test.py to fail Github Action

--- src/test.py
+++ src/test.py
@@ -5,7 +5,7 @@ import time
 import io
 from pathlib import Path
 from content_writer import ContentWriter
-import util as util
+import util
 
 class TestUtil(unittest.TestCase):
     #Tests for check_exts_to_include
@@ -102,7 +102,7 @@ class TestContentWriter(unittest.TestCase):
         )
 
         output = ostream.getvalue()
-        self.assertIn("## Summary", output)
+        self.assertIn("## Summary1", output)
         self.assertIn("- Total files: 3", output)
         self.assertIn("- Total recent files: 2", output)
         self.assertIn("- Total lines: 50", output)
